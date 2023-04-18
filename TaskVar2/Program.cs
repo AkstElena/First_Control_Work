128e40b979a796bdf4e50358902d626f7a308e91
Akst Elena <akstelena@yandex.ru>
2023-04-18 09:47:44 +0300

    Add Variant2

---
+++ TaskVar2/Program.cs
@@ -0,0 +1,22 @@
+﻿string [] array = {"5","2","3","7","1","4","6"};
+Console.WriteLine($"Исходный массив строк:  [\"{string.Join("\", \"",array)}\"]");
+int size = array.Length;
+int newSize = new Random().Next(0, 4);
+Console.WriteLine($"Количество элементов в уменьшенном массиве (от 0 до 3 включительно): {newSize}");
+
+string [] SmallArray(string [] array)
+{
+  for (int j = 0; j < size-newSize; j++)
+  {
+    int indexDel=new Random().Next(0, array.Length);
+    string [] newArray = new string [array.Length-1];
+    for (int i = 0; i < indexDel; i++)
+      newArray[i] = array[i];      
+    for (int i = indexDel; i < array.Length-1; i++) 
+      newArray[i] = array[i+1];     
+    array = newArray;
+  }
+  return array;
+}
+
+Console.WriteLine($"Итоговый массив строк:  [\"{string.Join("\", \"",SmallArray(array))}\"]");
